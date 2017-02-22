06f5b6d32f70c48740460e7f28212b1a4aeafe5c
Pankaj Singh <pankaj_arunsingh@ymail.com>
2017-02-22 16:10:17 +1100

    handling data flow and props

--- app/app.js
+++ app/app.js
@@ -1,22 +1,36 @@
 var React=require('react');
 var ReactDOM=require('react-dom');
 
+var samples=require('./sample-data');
 var App=React.createClass({
+  getInitialState:function(){
+    return{
+      "humans":{},
+      "store":{}
+    }
+  },
+  loadSampleData:function(){
+    this.setState(samples);
+  },
   render:function(){
     return(
       <div>
         <div id="header"></div>
+        <button onClick={this.loadSampleData}>Load Sample Data</button>
         <div className="container"></div>
       <div className="column"></div>
        <div className="column"></div>
         <div className="column"></div>
-      <InboxPane/>
+      <InboxPane humans={this.state.humans}/>
         </div>
     );
   }
 });
 
 var InboxPane=React.createClass({
+  renderInboxItem:function(human){
+    return <InboxItem key={human} index={human} details={this.props.human[human]}/>
+  },
   render:function(){
     return(
     <div id="inbox-pane">
@@ -30,7 +44,7 @@ var InboxPane=React.createClass({
           </tr>
         </thead>
         <tbody>
-          <InboxItem/>
+          {Object.keys(this.props.humans).map(this.renderInboxItem)}
         </tbody>
       </table>
     </div>
@@ -38,15 +52,21 @@ var InboxPane=React.createClass({
   }
 });
 var InboxItem=React.createClass({
-render:function(){
-  return(
-    <tr>
-      <td>7PM</td>
-      <td>Pankaj Loves Pizza </td>
-      <td>Confirmed </td>
+sortByDate: function(a, b) {
+      return a.time>b.time ? -1 : a.time<b.time ? 1 : 0;
+  },
+  messageSummary: function(conversations){
+    var lastMessage = conversations.sort(this.sortByDate)[0];
+    return lastMessage.who + ' said: "' + lastMessage.text + '" @ ' + lastMessage.time.toDateString();
+  },
+  render: function(){
+    return (
+      <tr>
+        <td>{this.messageSummary(this.props.details.conversations)}</td>
+        <td>{this.props.index}</td>
+        <td>{this.props.details.orders.sort(this.sortByDate)[0].status}</td>
       </tr>
-  );
-
-}
+    )
+  }
 });
 ReactDOM.render(<App/>,document.getElementById("main"));
